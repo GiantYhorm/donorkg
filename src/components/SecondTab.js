36cd9e5be12bd4835a74dc98bba535ddaa08f6cc
Daniyar Erkinov <dan4111k1@gmail.com>
2018-05-17 03:55:08 +0600

    SecondMain Recipient TabBar ended

--- src/components/SecondTab.js
+++ src/components/SecondTab.js
@@ -4,80 +4,129 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  Image
+  Image,
+  Dimensions,
+  Animated
 } from 'react-native';
 import { RED } from '../Variables';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+
+
+const initialLayout = {
+  height: 0,
+  width: Dimensions.get('window').width,
+};
+
+const FirstRoute = () =>
+  <View style={styles.inContainer}>
+    <View style={styles.centerArea}>
+      <View style={styles.requestCard}>
+        <View style={styles.userNameArea}>
+          <View style={styles.userName}>
+            <Image
+              style={styles.avatarImage}
+              source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+            />
+            <Text style={styles.nameText}>Name Surname </Text>
+          </View>
+          <Text style={styles.helpText}>
+            хочет помочь вам
+          </Text>
+        </View>
+        <View style={styles.rejectAcceptArea}>
+          <TouchableOpacity>
+            <Text style={styles.rejectText}>Отказаться</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.acceptText}>Принять</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.requestCard}>
+        <View style={styles.userNameArea}>
+          <View style={styles.userName}>
+            <Image
+              style={styles.avatarImage}
+              source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+            />
+            <Text style={styles.nameText}>Name Surname </Text>
+          </View>
+          <Text style={styles.helpText}>
+            хочет помочь вам
+          </Text>
+        </View>
+        <View style={styles.rejectAcceptArea}>
+          <TouchableOpacity>
+            <Text style={styles.rejectText}>Отказаться</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.acceptText}>Принять</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  </View>;
+const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
+const ThirdRoute = () => <View style={[ styles.container, { backgroundColor: 'cyan' } ]} />;
+
 
 class SecondTab extends Component {
+
+    state = {
+      index: 0,
+      routes: [
+        { key: 'first', title: 'В ОЖИДАНИИ' },
+        { key: 'second', title: 'РАССМОТРЕННЫЕ' },
+        { key: 'third', title: 'ДОНОРЫ' },
+      ],
+    };
+
+    _handleIndexChange = index => this.setState({ index });
+
+    _renderLabel = props => ({ route, index }) => {
+      const inputRange = props.navigationState.routes.map((x, i) => i);
+      const outputRange = inputRange.map(
+        inputIndex => (inputIndex === index ? RED : '#A1A1A3')
+      );
+      const color = props.position.interpolate({
+        inputRange,
+        outputRange,
+      });
+
+      return (
+        <Animated.Text style={[styles.labelStyle, { color }]}>
+          {route.title}
+        </Animated.Text>
+      );
+    };
+
+    _renderHeader = props => <TabBar
+      {...props}
+      indicatorStyle={styles.indicator}
+      style={styles.header}
+      renderLabel={this._renderLabel(props)}
+    />;
+
+    _renderScene = SceneMap({
+      first: FirstRoute,
+      second: SecondRoute,
+      third: ThirdRoute
+    });
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.topArea}>
-            <View style={styles.navBarTabActive}>
-              <Text style={styles.navBarTextActive}>В ОЖИДАНИИ</Text>
-            </View>
-            <View style={styles.navBarTabInactive}>
-              <Text style={styles.navBarTextInactive}>РАССМОТРЕНО</Text>
-            </View>
-            <View style={styles.navBarTabInactive}>
-              <Text style={styles.navBarTextInactive}>ДОНОРЫ</Text>
-            </View>
-          </View>
+        <TabViewAnimated
+          navigationState={this.state}
+          renderScene={this._renderScene}
+          renderHeader={this._renderHeader}
+          onIndexChange={this._handleIndexChange}
+          initialLayout={initialLayout}
+        />
 
-          <View style={styles.inContainer}>
-
-            <View style={styles.centerArea}>
-              <View style={styles.requestCard}>
-                <View style={styles.userNameArea}>
-                  <View style={styles.userName}>
-                    <Image
-                      style={styles.avatarImage}
-                      source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
-                    />
-                    <Text style={styles.nameText}>Name Surname </Text>
-                  </View>
-                  <Text style={styles.helpText}>
-                    хочет помочь вам
-                  </Text>
-                </View>
-                <View style={styles.rejectAcceptArea}>
-                  <TouchableOpacity>
-                    <Text style={styles.rejectText}>Отказаться</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Text style={styles.acceptText}>Принять</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-              <View style={styles.requestCard}>
-                <View style={styles.userNameArea}>
-                  <View style={styles.userName}>
-                    <Image
-                      style={styles.avatarImage}
-                      source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
-                    />
-                    <Text style={styles.nameText}>Name Surname </Text>
-                  </View>
-                  <Text style={styles.helpText}>
-                    хочет помочь вам
-                  </Text>
-                </View>
-                <View style={styles.rejectAcceptArea}>
-                  <TouchableOpacity>
-                    <Text style={styles.rejectText}>Отказаться</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Text style={styles.acceptText}>Принять</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
-            <TouchableOpacity style={styles.requestClose}>
-              <Text style={styles.requestCloseText}>Я больше не нуждаюсь в крови</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <TouchableOpacity style={styles.requestClose}>
+          <Text style={styles.requestCloseText}>Я больше не нуждаюсь в крови</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -94,48 +143,28 @@ const styles = StyleSheet.create({
     marginBottom: 10
   },
   header: {
-    flex: 1
+    backgroundColor: '#fff',
+    elevation: 0,
   },
-  topArea: {
-    flex: 1,
-    flexDirection: 'row',
-    marginVertical: 10,
-    marginHorizontal: 5
-  },
-  navBarTabActive: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 15,
-    borderBottomWidth: 3,
-    borderColor: RED,
-  },
-  navBarTabInactive: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 15,
-    borderBottomWidth: 3,
-    borderColor: 'transparent'
-  },
-  navBarTextActive: {
-    fontSize: 14,
+  labelStyle: {
     color: RED,
-  },
-  navBarTextInactive: {
-    fontSize: 14,
-    color: '#A1A1A3'
+    fontSize: 13,
+    paddingVertical: 10
   },
   centerArea: {
     flex: 15,
     marginVertical: 10,
   },
   requestClose: {
-    flex: 1,
+    position: 'absolute',
     backgroundColor: RED,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 25
+    borderRadius: 50,
+    alignSelf: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    bottom: 10
   },
   requestCloseText: {
     color: '#fff',
@@ -189,6 +218,13 @@ const styles = StyleSheet.create({
     color: 'green',
     padding: 5
   },
+  indicator: {
+    backgroundColor: RED,
+  },
+  tabBarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  }
 });
 
 export default SecondTab;
